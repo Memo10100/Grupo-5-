5ac7afd479bb0ed78e3081a1fa1cbe0f15b46d1b
juan gonzalez <juaniiiii1025@gmail.com>
2020-09-16 22:50:20 -0300

    Primer Commit

---
+++ Evaluacion 16 09 20/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Evaluacion_16_09_20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int opc = 0;
+            string salir;
+            int salir1;
+            Stack pila = new Stack();
+            double precio_total = 0;
+            int porcent_apli = 0;
+
+
+
+
+            Console.WriteLine("Camisas PRADBIT – Ventas minoristas y mayoristas\n");
+            do
+            {
+                int cant = pila.Count;
+                double precio_sin_descuento = cant * 1000;
+                if (cant < 4)
+                {
+                    precio_total = cant * 1000;
+                }
+
+                Console.Clear();
+                Console.WriteLine("---------------------------------------------------------------------------------------------------");
+                Console.WriteLine("MENÚ PRINCIPAL:");
+                Console.WriteLine("1 - Añadir camisa al carro de compras");
+                Console.WriteLine("2 - Eliminar camisa del carro de compras");
+                Console.WriteLine("3 - Salir\n");
+                Console.WriteLine("---------------------------------------------------------------------------------------------------");
+                Console.WriteLine("   -Cantidad de camisas en el carro de compras: " + cant);
+                Console.WriteLine("   - Precio unitario: $1000   ");
+                Console.WriteLine("   - Precio total sin descuento: "+precio_sin_descuento);
+                Console.WriteLine("   - Tipo de descuento aplicado: "+ porcent_apli + "%");
+                Console.WriteLine("   - Precio final con descuento: "+ precio_total);
+                Console.WriteLine("---------------------------------------------------------------------------------------------------");
+                Console.WriteLine("Ingrese una opción del menú: ");
+
+                opc = Convert.ToInt32(Console.ReadLine());
+
+                switch (opc)
+                {
+
+                    case 1:
+                        pila.Push(1000);
+                        
+                        Console.WriteLine("Objeto Añadido Al carrito");
+
+                        if (cant == 3 || cant >= 5){
+
+                            precio_total = precio_sin_descuento - ((precio_sin_descuento / 100)*10);
+                            porcent_apli = 10;
+
+                        } if(cant > 5){
+                            precio_total = precio_sin_descuento - ((precio_sin_descuento / 100) * 20);
+                            porcent_apli = 20;
+
+                        }
+                        break;
+                    case 2:
+                        int valor = 0;
+                        valor = (int)pila.Pop();
+                        Console.WriteLine("Objeto Eliminado Del carrito");
+                        break;
+                    case 3:
+                        Console.WriteLine("Realmente Deseea Salir... S/N");
+                        salir = Console.ReadLine();
+                        if (salir == "s" || salir == "S" );
+                        {
+                            salir1 = 1;
+                        }
+                        
+                        break;
+                }
+
+
+            } while (salir1 != 1);
+
+            Console.ReadKey();
+
+        }
+
+    }
+}
---
+++ Readme.txt
@@ -0,0 +1,23 @@
+PARTE 2 - Preguntas teóricas:
+
+A continuación, se listan algunas preguntas teóricas. Cada pregunta bien contestada suma 1p
+(un punto) y mal contestada resta 1p (un punto).
+
+1. ¿Qué diferencia hay entre un error de sintaxis y un error de lógica (falla)?
+
+Los errores de sintaxis son los más fácil de corregir y se muestran cuando el programa se compila, y de esa forma se pueden buscar fácilmente para corregirlos.
+Los errores de lógica se deben a que hemos creado nuestro algoritmo en algoritmo en forma errónea y aunque todas las sentencias estén correctas, el programa no 
+lleva a cabo lo que deseamos. Estos errores son más difíciles de corregir y la mejor forma de evitar es por medio de un análisis previo adecuado del problema a resolver.
+
+2. ¿Cuáles son los operadores de una expresión lógica?
+
+los operadores logicos son los siguientes : Y = "&&" , O = "| |" , no = "!"
+
+3. La refactorización de código: ¿Agrega/quita funcionalidad al programa?
+
+no, ya que como su significado indica, es un código alternativo tenga el mismo 
+comportamiento que el código de referencia. Sin modificar o alterar el comportamiento del codigo.
+
+4. Dentro de un ArrayList: ¿se pueden insertar valores de distinto tipo?
+
+Es una lista de elementos de tamaño dinámico. la lista puede almacenar distintos tipos de valores (int, string , float, etc.)
